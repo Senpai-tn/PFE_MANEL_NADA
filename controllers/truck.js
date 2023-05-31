c3878a107afbfdb395a13452d6d945fedf846a11
Khaled <khaledsahli36@gmail.com>
2023-05-31 19:48:37 +0100

    Truck

--- controllers/truck.js
+++ controllers/truck.js
@@ -16,12 +16,28 @@ router.get('/', async (req, res) => {
 
 //ajouter
 router.post('/', async (req, res) => {
-  const { model, serieNumber } = req.body
+  const { model, serieNumber, idFournisseur } = req.body
   const truck = new Truck({ model, serieNumber })
   await truck
     .save()
-    .then((savedTruck) => {
-      res.status(200).send(savedTruck)
+    .then(async (savedTruck) => {
+      const fournisseur = await User.findById(idFournisseur)
+      if (fournisseur.listeChauffeurs) {
+        fournisseur.listeChauffeurs.push(savedUser._id)
+      } else fournisseur.listeChauffeurs = [savedUser._id]
+      fournisseur
+        .save()
+        .then(async () => {
+          const clone = await User.findById(fournisseur._id)
+            .populate('listeCamions')
+            .populate('listeChauffeurs')
+            .populate('listeCommandes')
+          res.status(200).send(clone)
+        })
+        .catch((error) => {
+          console.log(error)
+          res.status(500).send(error)
+        })
     })
     .catch((error) => {
       res.status(500).send(error.message)
