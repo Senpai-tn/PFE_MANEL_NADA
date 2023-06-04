a4ef8aae0b55c9d5341aa8c550ecc04852fd816b
Khaled <khaledsahli36@gmail.com>
2023-06-04 02:45:34 +0100

    Update product

--- models/product.js
+++ models/product.js
@@ -2,6 +2,8 @@ const { Schema, model } = require('mongoose')
 const productSchema = new Schema({
   name: { type: String, required: true },
   categorie: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  deletedAt: { type: Date, default: null },
 })
 
 const Product = model('product', productSchema)
